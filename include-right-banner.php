6604b2c6c93447ce6b07be18f4cca9a5a5592163
sinsand <seen@startconcept.com>
2021-04-23 06:57:30 +0700

    update

--- include-right-banner.php
+++ include-right-banner.php
@@ -1,54 +1,40 @@
-<img src="<?=$page_link?>/images/support.jpg" title="¼ÙéÊ¹ÑºÊ¹Ø¹â¦É³ÒµÓáË¹è§¹Õé" alt="¼ÙéÊ¹ÑºÊ¹Ø¹â¦É³ÒµÓáË¹è§¹Õé" /><br /><br />
-	<? 
+<img src="<?php echo $page_link;?>/images/support.jpg" title="ï¿½ï¿½ï¿½Ê¹ÑºÊ¹Ø¹ï¿½ï¿½É³Òµï¿½ï¿½Ë¹è§¹ï¿½ï¿½" alt="ï¿½ï¿½ï¿½Ê¹ÑºÊ¹Ø¹ï¿½ï¿½É³Òµï¿½ï¿½Ë¹è§¹ï¿½ï¿½" /><br /><br />
+	<?php
 	$sqld = "SELECT * FROM banner where bPosition = 'D' and bStatus = '1' ORDER BY bDate_Create asc limit 0,6";
-	$resultd = $db->query($sqld);
-	$totalrowsd = mysql_num_rows($resultd);
+	$totalrowsd = select_num($sqld);
 	if($totalrowsd > 0){
-	$d = 200 ;
-	while($RWd = mysql_fetch_array($resultd)){
-	$bID[$d] = $RWd['bID'];
-	$bTitle[$d] = $RWd['bTitle'];
-	$bDetail[$d] = $RWd['bDetail'];
-	$bURL[$d] = $RWd['bURL'];
-	$bPic2[$d] = $RWd['bPic2'];
-	$bPic4[$d] = $RWd['bPic4'];
-	$bPic5[$d] = $RWd['bPic5'];
-	 $d++;}} 
+		$d = 200 ;
+		foreach (select_tb($sqld) as $RWd) {
+			$bID[$d] = $RWd['bID'];
+			$bTitle[$d] = $RWd['bTitle'];
+			$bDetail[$d] = $RWd['bDetail'];
+			$bURL[$d] = $RWd['bURL'];
+			$bPic2[$d] = $RWd['bPic2'];
+			$bPic4[$d] = $RWd['bPic4'];
+			$bPic5[$d] = $RWd['bPic5'];
+			$d++;
+		}
+	}
 	 ?>
-     <? if($bPic2[201] != ""){ ?><a href="<? echo $bURL[201]; ?> " title="<? echo $bTitle[201]; ?>" target="_blank"><? } ?><img src="<?=$page_link?>/picture_banner/banner-center/<? echo $bPic2[201]; ?>" width="125" border="0" alt="<? echo $bDetail[201]; ?>" title="<? echo $bDetail[201]; ?>" class="boarderimg" /><? if($bPic2[201] != ""){ ?></a><? } ?><br /><br />
-     <? if($bPic2[200] != ""){ ?><a href="<? echo $bURL[200]; ?> " title="<? echo $bTitle[200]; ?>" target="_blank"><? } ?><img src="<?=$page_link?>/picture_banner/banner-center/<? echo $bPic2[200]; ?>" border="0" width="125" height="125" alt="<? echo $bDetail[200]; ?>" title="<? echo $bDetail[200]; ?>" class="boarderimg" /><? if($bPic2[200] != ""){ ?></a><? } ?><br /><br />
-     <? if($bPic2[202] != "" ){ ?><a href="<? echo $bURL[202]; ?> " title="<? echo $bTitle[202]; ?>" target="_blank"><? } ?><img src="<?=$page_link?>/picture_banner/banner-center/<? echo $bPic2[202]; ?>" border="0" alt="<? echo $bDetail[202]; ?>" title="<? echo $bDetail[202]; ?>" class="boarderimg" /><? if($bPic2[202] != ""){ ?></a><? } ?><br /><br />
-     <? if($bPic2[203] != "" ){ ?><a href="<? echo $bURL[203]; ?> " title="<? echo $bTitle[203]; ?>" target="_blank"><? } ?><img src="<?=$page_link?>/picture_banner/banner-center/<? echo $bPic2[203]; ?>" border="0" alt="<? echo $bDetail[203]; ?>" title="<? echo $bDetail[203]; ?>" class="boarderimg" /><? if($bPic2[203] != ""){ ?></a><? } ?><br /><br />
-     <? if($bPic2[204] != ""){ ?><a href="<? echo $bURL[204]; ?> " title="<? echo $bTitle[204]; ?>" target="_blank"><? } ?><img src="<?=$page_link?>/picture_banner/banner-center/<? echo $bPic2[204]; ?>" border="0" alt="<? echo $bDetail[204]; ?>" title="<? echo $bDetail[204]; ?>" class="boarderimg" /><? if($bPic2[204] != ""){ ?></a><? } ?><br /><br />
-     <? if($bPic2[205] != ""){ ?><a href="<? echo $bURL[205]; ?> " title="<? echo $bTitle[205]; ?>" target="_blank"><? } ?><img src="<?=$page_link?>/picture_banner/banner-center/<? echo $bPic2[205]; ?>" border="0" alt="<? echo $bDetail[205]; ?>" title="<? echo $bDetail[205]; ?>" class="boarderimg" /><? if($bPic2[205] != ""){ ?></a><? } ?><br />
+     <?php if($bPic2[201] != ""){ ?><a href="<?php echo $bURL[201]; ?> " title="<?php echo $bTitle[201]; ?>" target="_blank"><?php } ?><img src="<?php echo $page_link?>/picture_banner/banner-center/<?php echo $bPic2[201]; ?>" width="125" border="0" alt="<?php echo $bDetail[201]; ?>" title="<?php echo $bDetail[201]; ?>" class="boarderimg" /><?php if($bPic2[201] != ""){ ?></a><?php } ?><br /><br />
+     <?php if($bPic2[200] != ""){ ?><a href="<?php echo $bURL[200]; ?> " title="<?php echo $bTitle[200]; ?>" target="_blank"><?php } ?><img src="<?php echo $page_link?>/picture_banner/banner-center/<?php echo $bPic2[200]; ?>" border="0" width="125" height="125" alt="<?php echo $bDetail[200]; ?>" title="<?php echo $bDetail[200]; ?>" class="boarderimg" /><?php if($bPic2[200] != ""){ ?></a><?php } ?><br /><br />
+     <?php if($bPic2[202] != "" ){ ?><a href="<?php echo $bURL[202]; ?> " title="<?php echo $bTitle[202]; ?>" target="_blank"><?php } ?><img src="<?php echo $page_link?>/picture_banner/banner-center/<?php echo $bPic2[202]; ?>" border="0" alt="<?php echo $bDetail[202]; ?>" title="<?php echo $bDetail[202]; ?>" class="boarderimg" /><?php if($bPic2[202] != ""){ ?></a><?php } ?><br /><br />
+     <?php if($bPic2[203] != "" ){ ?><a href="<?php echo $bURL[203]; ?> " title="<?php echo $bTitle[203]; ?>" target="_blank"><?php } ?><img src="<?php echo $page_link?>/picture_banner/banner-center/<?php echo $bPic2[203]; ?>" border="0" alt="<?php echo $bDetail[203]; ?>" title="<?php echo $bDetail[203]; ?>" class="boarderimg" /><?php if($bPic2[203] != ""){ ?></a><?php } ?><br /><br />
+     <?php if($bPic2[204] != ""){ ?><a href="<?php echo $bURL[204]; ?> " title="<?php echo $bTitle[204]; ?>" target="_blank"><?php } ?><img src="<?php echo $page_link?>/picture_banner/banner-center/<?php echo $bPic2[204]; ?>" border="0" alt="<?php echo $bDetail[204]; ?>" title="<?php echo $bDetail[204]; ?>" class="boarderimg" /><?php if($bPic2[204] != ""){ ?></a><?php } ?><br /><br />
+     <?php if($bPic2[205] != ""){ ?><a href="<?php echo $bURL[205]; ?> " title="<?php echo $bTitle[205]; ?>" target="_blank"><?php } ?><img src="<?php echo $page_link?>/picture_banner/banner-center/<?php echo $bPic2[205]; ?>" border="0" alt="<?php echo $bDetail[205]; ?>" title="<?php echo $bDetail[205]; ?>" class="boarderimg" /><?php if($bPic2[205] != ""){ ?></a><?php } ?><br />
 	 <p align="center"><font color="#B4B4B4">---------------------------------------------</font></p>
-<br />
-<center><script type="text/javascript"><!--
-google_ad_client = "pub-2101138700314615";
-/* 200x90, ¶Ù¡ÊÃéÒ§¢Öé¹áÅéÇ 7/9/10 */
-google_ad_slot = "7903745120";
-google_ad_width = 200;
-google_ad_height = 90;
-//-->
-</script>
-<script type="text/javascript"
-src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-</script></center>
-<br />
-<img src="<?=$page_link?>/images/network.jpg" title="áÅ¡ÅÔé§¤ìà¾×èÍ¹ºéÒ¹" alt="áÅ¡ÅÔé§¤ìà¾×èÍ¹ºéÒ¹" /><br /><br />
-	<?
+
+<img src="<?php echo $page_link;?>/images/network.jpg" title="ï¿½Å¡ï¿½ï¿½é§¤ï¿½ï¿½ï¿½ï¿½ï¿½Í¹ï¿½ï¿½Ò¹" alt="ï¿½Å¡ï¿½ï¿½é§¤ï¿½ï¿½ï¿½ï¿½ï¿½Í¹ï¿½ï¿½Ò¹" /><br /><br />
+	<?php
 $sqlw = "SELECT * FROM web_link where wStatus = '1' ORDER BY RAND() limit 0,10";
-$resultw = $db->query($sqlw);
-while ($roww = mysql_fetch_array($resultw))
-{	
-if($roww['wPic'] != "")
-{
-echo "<a href='$roww[wURL]' title='$roww[wDetail]'  target='_blank'><img src='".$page_link."/administrator/weblink/$roww[wPic]' alt='$roww[wDetail]' title='$roww[wDetail]' class='boarderimg' /></a>";
-}
-?>
-<br />&nbsp;<br />
-<?
+foreach (select_tb($sqlw) as $roww) {
+	if($roww['wPic'] != ""){
+		echo "<a href='$roww[wURL]' title='$roww[wDetail]'  target='_blank'><img src='".$page_link."/administrator/weblink/$roww[wPic]' alt='$roww[wDetail]' title='$roww[wDetail]' class='boarderimg' /></a>";
+	}
+	?>
+	<br />&nbsp;<br />
+	<?php
 }
 ?>&nbsp;<br />
-<a href="<?=$page_link?>/áÅ¡ÅÔé§¤ìà¾×èÍ¹ºéÒ¹/link-network>">à¾ÔèÁÅÔé§¤ì¤ÅÔ¡</a>
+<a href="<?php echo $page_link;?>/ï¿½Å¡ï¿½ï¿½é§¤ï¿½ï¿½ï¿½ï¿½ï¿½Í¹ï¿½ï¿½Ò¹/link-network>">ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½é§¤ï¿½ï¿½ï¿½Ô¡</a>
 	 <p align="center"><font color="#B4B4B4">---------------------------------------------</font></p>
--- index.php
+++ index.php
@@ -58,7 +58,7 @@ function bookmark(title,url){
 <center>
 </center><br /><br />
 <a href="http://www.startconcept.com/" target="_blank"><img src="http://image.ohozaa.com/i6/yenta4.png" alt="Manga ???? Onepiece" class="b-s-ora"width="1" height="1" border="0" /></a>
-<img src="images/20ID.jpg" /><?php include "include-top10-type.php"; ?><p class="alignrights"><a href="<?php echo $page_link?>/property-search.php" title="??ѧ???????Ѿ???????"><strong>??ѧ???????Ѿ???????</strong></a></p>&nbsp;</div>
+<img src="images/20ID.jpg" /><?php include "include-top10-type.php"; ?><p class="alignrights"><a href="<?php echo $page_link;?>/property-search.php" title="??ѧ???????Ѿ???????"><strong>??ѧ???????Ѿ???????</strong></a></p>&nbsp;</div>
 <div class="rjobright"><?php include "include-right-banner.php"; ?></div>
 
 </div>
