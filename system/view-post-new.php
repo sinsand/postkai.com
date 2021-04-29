6ddfe1b4b207d6218e0eefa83a903e3674039a54
sinsand <seen@startconcept.com>
2021-04-29 12:27:31 +0700

    up

--- system/view-post-new.php
+++ system/view-post-new.php
@@ -1,11 +1,11 @@
 <h2 class="main-head-cate t-announce f-k">เพิ่มประกาศใหม่</h2>
 <div class="col-xs-12">
-  <form class="form-horizontal" action="<?php echo $LinkPath;?>" method="post">
+  <form class="" action="<?php echo $LinkPath;?>" method="post">
 
     <div class="row">
       <div class="col-xs-12" style="padding:25px 15px;">
-        <div class="form-group">
-          <label class="control-label col-sm-3 text-ll" for="post_category">หมวดหมู่:</label>
+        <div class="form-group col-xs-12">
+          <label class="control-label" for="post_category">หมวดหมู่:</label>
           <div class="col-sm-6 col-xs-12">
             <select class="form-control" name="post_category" id="post_category">
               <option value="">เลิอกหมวดหมู่</option>
@@ -22,7 +22,7 @@
             </select>
           </div>
         </div>
-        <div class="form-group">
+        <div class="form-group col-xs-12">
           <label class="control-label col-sm-3 text-ll" for="province">จังหวัด:</label>
           <div class="col-sm-6 col-xs-12">
             <select class="form-control" name="province">
@@ -40,7 +40,7 @@
             </select>
           </div>
         </div>
-        <div class="form-group">
+        <div class="form-group col-xs-12">
           <label class="control-label col-sm-3 text-ll" for="post_type">ประเภท:</label>
           <div class="col-sm-6 col-xs-12">
             <select class="form-control" name="post_type">
@@ -58,19 +58,19 @@
             </select>
           </div>
         </div>
-        <div class="form-group">
+        <div class="form-group col-xs-12">
           <label class="control-label col-sm-3 text-ll" for="post_subject">หัวข้อประกาศ:</label>
           <div class="col-sm-9">
             <input class="form-control" type="text" name="post_subject" value="" placeholder="">
           </div>
         </div>
-        <div class="form-group">
+        <div class="form-group col-xs-12">
           <label class="control-label col-sm-3 text-ll" for="post_desc">รายละเอียดย่อ:</label>
           <div class="col-sm-9">
             <input class="form-control" type="text" name="post_desc" value="" placeholder="">
           </div>
         </div>
-        <div class="form-group">
+        <div class="form-group col-xs-12">
           <label class="control-label col-sm-3 text-ll mb-5" for="post_desc_full">รายละเอียดทั้งหมด:</label>
           <div class="col-sm-12">
             <textarea class="form-control summernote" name="post_desc_full"></textarea>
