4b039b737f70009e39caf583801cd5e7d701523c
sinsand <seen@startconcept.com>
2021-04-29 12:30:26 +0700

    up

--- system/view-post-new.php
+++ system/view-post-new.php
@@ -5,8 +5,8 @@
     <div class="row">
       <div class="col-xs-12" style="padding:25px 0px;">
         <div class="form-group col-xs-12 pr-0 pl-0">
-          <label class="control-label col-sm-3 text-ll" for="post_category">หมวดหมู่:</label>
-          <div class="col-sm-6 col-xs-12">
+          <label class="control-label col-sm-3 text-ll pr-0 pl-0" for="post_category">หมวดหมู่:</label>
+          <div class="col-sm-6 col-xs-12 pr-0 pl-0">
             <select class="form-control" name="post_category" id="post_category">
               <option value="">เลิอกหมวดหมู่</option>
               <?php
@@ -23,8 +23,8 @@
           </div>
         </div>
         <div class="form-group col-xs-12 pr-0 pl-0">
-          <label class="control-label col-sm-3 text-ll" for="province">จังหวัด:</label>
-          <div class="col-sm-6 col-xs-12">
+          <label class="control-label col-sm-3 text-ll pr-0 pl-0" for="province">จังหวัด:</label>
+          <div class="col-sm-6 col-xs-12 pr-0 pl-0">
             <select class="form-control" name="province">
               <option value="">เลิอกจังหวัด</option>
               <?php
@@ -41,8 +41,8 @@
           </div>
         </div>
         <div class="form-group col-xs-12 pr-0 pl-0">
-          <label class="control-label col-sm-3 text-ll" for="post_type">ประเภท:</label>
-          <div class="col-sm-6 col-xs-12">
+          <label class="control-label col-sm-3 text-ll pr-0 pl-0" for="post_type">ประเภท:</label>
+          <div class="col-sm-6 col-xs-12 pr-0 pl-0">
             <select class="form-control" name="post_type">
               <option value="">เลิอกประเภท</option>
               <?php
@@ -59,20 +59,20 @@
           </div>
         </div>
         <div class="form-group col-xs-12 pr-0 pl-0">
-          <label class="control-label col-sm-3 text-ll" for="post_subject">หัวข้อประกาศ:</label>
-          <div class="col-sm-9">
+          <label class="control-label col-sm-3 text-ll pr-0 pl-0" for="post_subject">หัวข้อประกาศ:</label>
+          <div class="col-sm-9 pr-0 pl-0">
             <input class="form-control" type="text" name="post_subject" value="" placeholder="">
           </div>
         </div>
         <div class="form-group col-xs-12 pr-0 pl-0">
-          <label class="control-label col-sm-3 text-ll" for="post_desc">รายละเอียดย่อ:</label>
-          <div class="col-sm-9">
+          <label class="control-label col-sm-3 text-ll pr-0 pl-0" for="post_desc">รายละเอียดย่อ:</label>
+          <div class="col-sm-9 pr-0 pl-0">
             <input class="form-control" type="text" name="post_desc" value="" placeholder="">
           </div>
         </div>
         <div class="form-group col-xs-12 pr-0 pl-0">
-          <label class="control-label col-sm-3 text-ll mb-5" for="post_desc_full">รายละเอียดทั้งหมด:</label>
-          <div class="col-sm-12">
+          <label class="control-label col-sm-3 text-ll mb-5 pr-0 pl-0" for="post_desc_full">รายละเอียดทั้งหมด:</label>
+          <div class="col-sm-12 pr-0 pl-0">
             <textarea class="form-control summernote" name="post_desc_full"></textarea>
           </div>
         </div>
