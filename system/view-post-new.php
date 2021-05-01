8756cf092d5c114181728a95ccbbd002f1b54b3c
sinsand <seen@startconcept.com>
2021-05-01 21:31:01 +0700

    up

--- system/view-post-new.php
+++ system/view-post-new.php
@@ -124,16 +124,16 @@
         <div class="form-group col-xs-12 pr-0 pl-0">
           <label class="control-label col-sm-3 text-ll pr-0 pl-0" for="post_subject">แนบรูปภาพ <br>(.jpg .jpeg หรือ .png เท่านั้น)<br> ขนาดเหมาะสม คือ 1:1,800x800px</label>
           <div class="col-sm-9 pr-0 pl-0">
-            <div class="col-xs-12">
+            <div class="col-xs-12 p-0 ">
               <input id="fileshow-1" name="fileshow-1" type="file" class="filestyle" required  data-buttonName="btn-primary">
             </div>
-            <div class="col-xs-12 pt-5">
+            <div class="col-xs-12 pt-5 pl-0 pr-0">
               <input id="fileshow-2" name="fileshow-2" type="file" class="filestyle"  data-buttonName="btn-primary">
             </div>
-            <div class="col-xs-12 pt-5">
+            <div class="col-xs-12 pt-5 pl-0 pr-0">
               <input id="fileshow-3" name="fileshow-3" type="file" class="filestyle"  data-buttonName="btn-primary">
             </div>
-            <div class="col-xs-12 pt-5">
+            <div class="col-xs-12 pt-5 pl-0 pr-0">
               <input id="fileshow-4" name="fileshow-4" type="file" class="filestyle" data-buttonName="btn-primary">
             </div>
           </div>
