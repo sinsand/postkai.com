ab78d73fb3bcda7056a6d44a2283a3e3acea36ee
sinsand <seen@startconcept.com>
2021-05-02 16:54:34 +0700

    up

--- view-sitemap.php
+++ view-sitemap.php
@@ -1,3 +1,5 @@
+<sitemapindex xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/siteindex.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<sitemap>
 <?php
 header('Content-Type:text/xml');
 echo "<?xml version='1.0' encoding='UTF-8'?>"."\n";
@@ -59,3 +61,5 @@ if (select_num($SqlSelectPost)>0) {
 }
 echo "</urlset>";
 ?>
+</sitemap>
+</sitemapindex>
