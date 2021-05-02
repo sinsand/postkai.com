250e3a0c0f08a28e0908061c258e5d168cd81574
sinsand <seen@startconcept.com>
2021-05-02 16:41:07 +0700

    up

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 include("config/config.php");
 
 if ($UrlPage!="") {
-  include("system/index.php");
+  if ($UrlPage=="sitemap.xml") {
+    include("view-sitemap.php");
+  }else {
+    include("system/index.php");
+  }
 }else {
   include("system/index.php");
   //include("under.php");
---
+++ view-sitemap.php
@@ -0,0 +1,47 @@
+<?php
+header('Content-Type:text/xml');
+echo "<?xml version='1.0' encoding='UTF-8'?>"."\n";
+echo "<urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'>"."\n";
+$SqlSelectPost = "SELECT *
+                  FROM sb_job sj
+                  WHERE ( sj.jStatus = '1' )
+                  ORDER BY sj.jDate_Create ASC";
+
+if (select_num($SqlSelectPost)>0) {
+  echo "<url>
+          <loc>".$LinkWeb."</loc>
+        </url>";
+  echo "<url>
+          <loc>".$LinkWeb."search</loc>
+        </url>";
+  echo "<url>
+          <loc>".$LinkWeb."register</loc>
+        </url>";
+  echo "<url>
+          <loc>".$LinkWeb."login</loc>
+        </url>";
+  echo "<url>
+          <loc>".$LinkWeb."post-new</loc>
+        </url>";
+  echo "<url>
+          <loc>".$LinkWeb."policy</loc>
+        </url>";
+  echo "<url>
+          <loc>".$LinkWeb."term-and-condition</loc>
+        </url>";
+  echo "<url>
+          <loc>".$LinkWeb."</loc>
+          <lastmod>2021-04-27T20:18:04+00:00</lastmod>
+          <changefreq>daily</changefreq>
+        </url>";
+
+  foreach (select_tb($SqlSelectPost) as $row) {
+    echo "<url>";
+    echo "<loc>".$LinkWeb."post/".$row['link']."</loc>";
+    echo "<lastmod>".$row['jDate_Create']."</lastmod>";
+    echo "<changefreq>daily</changefreq>";
+    echo "</url>";
+  }
+}
+echo "</urlset>";
+?>
