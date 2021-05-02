737a06967d7af857187bebb6038feec455ca5c08
sinsand <seen@startconcept.com>
2021-05-02 16:53:04 +0700

    up

--- view-sitemap.php
+++ view-sitemap.php
@@ -1,5 +1,5 @@
 <?php
-//header('Content-Type:text/xml');
+header('Content-Type:text/xml');
 echo "<?xml version='1.0' encoding='UTF-8'?>"."\n";
 echo "<urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'>"."\n";
 $SqlSelectPost = "SELECT *
@@ -10,30 +10,44 @@ $SqlSelectPost = "SELECT *
 if (select_num($SqlSelectPost)>0) {
   echo "<url>
           <loc>".$LinkWeb."</loc>
-        </url>";
-  echo "<url>
-          <loc>".$LinkWeb."search</loc>
-        </url>";
-  echo "<url>
-          <loc>".$LinkWeb."register</loc>
-        </url>";
-  echo "<url>
-          <loc>".$LinkWeb."login</loc>
-        </url>";
-  echo "<url>
-          <loc>".$LinkWeb."post-new</loc>
-        </url>";
-  echo "<url>
-          <loc>".$LinkWeb."policy</loc>
-        </url>";
-  echo "<url>
-          <loc>".$LinkWeb."term-and-condition</loc>
-        </url>";
-  echo "<url>
-          <loc>".$LinkWeb."</loc>
           <lastmod>2021-04-27T20:18:04+00:00</lastmod>
           <changefreq>daily</changefreq>
         </url>";
+  echo "<url>
+          <loc>".$LinkWeb."search</loc>
+          <lastmod>2021-04-27T20:18:04+00:00</lastmod>
+          <changefreq>daily</changefreq>
+        </url>";
+  echo "<url>
+          <loc>".$LinkWeb."register</loc>
+          <lastmod>2021-04-27T20:18:04+00:00</lastmod>
+          <changefreq>daily</changefreq>
+        </url>";
+  echo "<url>
+          <loc>".$LinkWeb."login</loc>
+          <lastmod>2021-04-27T20:18:04+00:00</lastmod>
+          <changefreq>daily</changefreq>
+        </url>";
+  echo "<url>
+          <loc>".$LinkWeb."post-new</loc>
+          <lastmod>2021-04-27T20:18:04+00:00</lastmod>
+          <changefreq>daily</changefreq>
+        </url>";
+  echo "<url>
+          <loc>".$LinkWeb."policy</loc>
+          <lastmod>2021-04-27T20:18:04+00:00</lastmod>
+          <changefreq>daily</changefreq>
+        </url>";
+  echo "<url>
+          <loc>".$LinkWeb."term-and-condition</loc>
+          <lastmod>2021-04-27T20:18:04+00:00</lastmod>
+          <changefreq>daily</changefreq>
+        </url>";
+  /*echo "<url>
+          <loc>".$LinkWeb."</loc>
+          <lastmod>2021-04-27T20:18:04+00:00</lastmod>
+          <changefreq>daily</changefreq>
+        </url>";*/
 
   foreach (select_tb($SqlSelectPost) as $row) {
     echo "<url>";
