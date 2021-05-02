d37a1773428df05af17de5f873c3bd04562716be
sinsand <seen@startconcept.com>
2021-05-02 16:52:01 +0700

    up

--- view-sitemap.php
+++ view-sitemap.php
@@ -1,5 +1,5 @@
 <?php
-header('Content-Type:text/xml');
+//header('Content-Type:text/xml');
 echo "<?xml version='1.0' encoding='UTF-8'?>"."\n";
 echo "<urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'>"."\n";
 $SqlSelectPost = "SELECT *
