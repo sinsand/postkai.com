5c78f6a2a7d047df0b28258000f5fc66ae5a4afa
sinsand <seen@startconcept.com>
2021-05-03 15:30:49 +0700

    up

--- system/view-post-detail.php
+++ system/view-post-detail.php
@@ -186,7 +186,7 @@
             </div>
           </div>
           <div class="col-xs-12 p-0">
-            <h3 class="main-min-cate-show t-others f-k">รายละเอียด</h3>
+            <h3 class="main-min-cate t-others f-k">รายละเอียด</h3>
             <div class="row mt-10 mb-10"><?php echo htmlspecialchars_decode($row['jDetail']);?></div>
           </div>
         </div>
