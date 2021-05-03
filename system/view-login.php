5f706c6d708c6a0e6e02273cc7770ab2a19cebdb
sinsand <seen@startconcept.com>
2021-05-03 09:10:51 +0700

    up

--- system/view-login.php
+++ system/view-login.php
@@ -5,13 +5,13 @@
       <div class="form-group">
         <label class="control-label col-sm-2" for="email">Email:</label>
         <div class="col-sm-10">
-          <input type="email" class="form-control" id="email" placeholder="Enter email">
+          <input type="email" class="form-control" id="email" placeholder="Enter email" required autocomplete="off">
         </div>
       </div>
       <div class="form-group">
         <label class="control-label col-sm-2" for="password">Password:</label>
         <div class="col-sm-10">
-          <input type="password" class="form-control" id="password" placeholder="Enter password">
+          <input type="password" class="form-control" id="password" placeholder="Enter password" required autocomplete="off">
         </div>
       </div>
       <div class="form-group">
