e00db046cb7dd7a6c860716be26027f6047326e1
sinsand <seen@startconcept.com>
2021-05-03 09:08:31 +0700

    up

--- system/view-login.php
+++ system/view-login.php
@@ -1,1 +1,25 @@
 <h2 class="main-head-cate t-search f-k">เข้าสู่ระบบ</h2>
+<div class="row">
+  <div class="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2">
+    <form action="<?php echo $LinkPath;?>" method="post">
+      <div class="form-group">
+        <label class="control-label col-sm-2" for="email">Email:</label>
+        <div class="col-sm-10">
+          <input type="email" class="form-control" id="email" placeholder="Enter email">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="control-label col-sm-2" for="password">Password:</label>
+        <div class="col-sm-10">
+          <input type="password" class="form-control" id="password" placeholder="Enter password">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="control-label col-sm-2" for="email"></label>
+        <div class="col-sm-10">
+          <button type="submit" name="btnlogin" class="btn btn-success">เข้าสูระบบ</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
