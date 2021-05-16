fc515c029baf6612e675ee769f0244788aac4bd4
sinsand <seen@startconcept.com>
2021-05-16 14:50:45 +0700

    up

---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+include('config/config.php');
+
+$SqlSelect = "SELECT a.jProvince,b.PROVINCE_ID FROM sb_job a INNER JOIN p_province b ON (a.jProvince = b.PROVINCE_NAME) ORDER BY a.jProvince ASC";
+foreach (select_tb($SqlSelect) as $value) {
+  echo "<p> UPDATE sb_job SET jProvince = '".$value['PROVINCE_ID']."' WHERE jID = '".$value['jID']."'; </p>";
+}
+
+?>
