8cb2be6e191893b3f9e3ecb6710b8dddf3ae7625
sinsand <seen@startconcept.com>
2021-05-16 14:21:35 +0700

    update

--- config/function.php
+++ config/function.php
@@ -1,4 +1,5 @@
 <?php
+////  for xml file
 function con_xml($value){
   $hour = substr($value,12,2);
   $min = substr($value,15,2);
@@ -9,10 +10,58 @@ function con_xml($value){
   return date("Y-m-d\TH:i:s+00:00",mktime($hour,$min,$second,$month,$day,$year));
   //2009-12-27 00:10:34
 }
-
+//// time thai full
 function day_format_month_thai($value){
   $a_monthth = array("มกราคม","กุมภาพันธ์","มีนาคม","เมษายน","พฤษภาคม","มิถุนายน","กรกฎาคม","สิงหาคม","กันยายน","ตุลาคม","พฤศจิกายน","ธันวาคม");
   return substr($value,8,2)." ".$a_monthth[substr($value,6,2)-1]." ".(substr($value,0,4)+543). " ".substr($value,11,8);
 }
 
+//// alert success
+function fSuccess($type,$value,$link,$time){
+  ?>
+  <div class="alert alert-success alert-dismissible">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+    <?php echo FType($type);?>
+    <?php echo $value;?> ....<i class="fas fa-spinner faa-spin animated"></i>
+  </div>
+  <?php
+    if ($time>0) {
+      ?><meta http-equiv="refresh" content="<?php echo $time;?>;url=<?php echo $link;?>"><?php
+    }
+}
+//// alert error
+function fError($type,$value,$sql=""){
+  ?>
+  <div class="alert alert-warning alert-dismissible">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+    <?php echo FType($type);?>
+    <?php echo $value;?> ....<?php echo $sql;?>
+  </div>
+  <?php
+}
+//// check alert type
+function FType($type){
+  if ($type==1) { /// add
+    return "<h4><i class='far fa-check-circle'></i> ADD !</h4>";
+  }else if ($type==2){ /// update
+    return "<h4><i class='far fa-check-circle'></i> UPDATE !</h4>";
+  }else if ($type==3){ // delete
+    return "<h4><i class='far fa-check-circle'></i> DELETE !</h4>";
+  }else if ($type==4){ // sign out
+    return "<h4><i class='fas fa-lock-open'></i> Exit !</h4>";
+  }else if ($type==5){ // sign in
+    return "<h4><i class='fas fa-lock'></i> Sign In !</h4>";
+  }else if ($type==6){ // register
+    return "<h4><i class='far fa-check-circle'></i> Register !</h4>";
+  }
+}
+//// insert to table log
+function log_insert($value,$eid){
+  $SqlInsert  = "INSERT INTO p_log VALUES(0,".base64url_decode($eid).",'".htmlentities($value)."',now());";
+  if(insert_tb($SqlInsert)==true){
+    return true;
+  }else {
+    return false;
+  }
+}
 ?>
--- system/view-post-new.php
+++ system/view-post-new.php
@@ -1,10 +1,105 @@
 <h2 class="main-head-cate t-announce f-k">เพิ่มประกาศใหม่</h2>
 <?php
   if (isset($_POST['btnPost'])) {
-    // code...
+    $v_cate = $_POST['post_category'];
+    $v_province = $_POST['post_province'];
+    $v_type = $_POST['post_type'];
+    $v_subject = htmlspecialchars($_POST['post_subject'],ENT_QUOTES);
+    $v_detail = htmlspecialchars($_POST['post_desc'],ENT_QUOTES);
+    $v_description = htmlspecialchars($_POST['post_desc_full'],ENT_QUOTES);
+    $v_price = $_POST['post_price'];
+    $v_type_product = $_POST['post_product'];
+    $v_day = $_POST['post_day'];
+    $v_comment = $_POST['post_comment'];
+
+    $v_photo1 = "";
+    $v_photo2 = "";
+    $v_photo3 = "";
+    $v_photo4 = "";
+    $v_photo5 = "";
+
+    $n_name = htmlspecialchars($_POST['post_n_fullname'],ENT_QUOTES);
+    $n_address = htmlspecialchars($_POST['post_n_address'],ENT_QUOTES);
+    $n_province = $_POST['post_n_province'];
+    $n_phone = htmlspecialchars($_POST['post_n_telephone'],ENT_QUOTES);
+    $n_email = htmlspecialchars($_POST['post_n_email'],ENT_QUOTES);
+    $n_line = htmlspecialchars($_POST['post_n_lineid'],ENT_QUOTES);
+    $n_code_edit = $_POST['post_code_edit'];
+
+    $v_mID = "";
+    $d_mID = "";
+    if (!empty($_COOKIE['mID'])) {
+      $v_mID = ",mID";
+      $m_mID = "'".base64url_decode($_COOKIE['mID'])."'";
+    }
+
+    for ($i=1; $i <= count($_FILES['fileshow']['name']); $i++) {
+      if ($_FILES['fileshow']['name']!="") {
+        $temp      = explode(".", $_FILES["fileshow"]["name"][$i]);
+        $imagename = $_FILES['fileshow']['name'][$i];
+        $source    = $_FILES['fileshow']['tmp_name'][$i];
+        $target    = "images/post/picture_job_".$i."/".$imagename;
+        move_uploaded_file($source, $target);
+
+        $imagepath = date("Y-m-d_His").".".$temp;
+        $save      = "images/post/picture_job_".$i."/".$imagename; //This is the new file you saving
+        $file      = "images/post/picture_job_".$i."/".$imagepath; //This is the original file
+
+        list($width, $height) = getimagesize($target);
+
+        $modwidth  = 1000;
+        $diff      = $width / $modwidth;
+        $modheight = $height / $diff;
+
+        $tn        = imagecreatetruecolor($modwidth, $modheight) ;
+        $image     = imagecreatefromjpeg($file) ;
+        imagecopyresampled($tn, $image, 0, 0, 0, 0, $modwidth, $modheight, $width, $height) ;
+
+        imagejpeg($tn, $save, 100) ;
+        unlink($target); //Delete our uploaded file
+
+        if ($i==1) {
+          $v_photo1 = $imagepath;
+        }elseif ($i==2) {
+          $v_photo2 = $imagepath;
+        }elseif ($i==3) {
+          $v_photo3 = $imagepath;
+        }elseif ($i==4) {
+          $v_photo4 = $imagepath;
+        }else
+          $v_photo5 = $imagepath;
+        }
+
+
+      }
+    }
+
+
+
+    $SqlInsert = "INSERT INTO sb_job
+                  (jTitle,jDetail,jDesc,jPrice,jaType,jType,jProvince,jPic1,jPic2,jPic3,jPic4,jPic5,
+                   jc_Name,jc_Address,jc_Province,jc_Telephone,jc_Email,jRead,jDate_Create,jStatus,
+                   jComment,jTypeProduct,jPostDay,jLINEID,jEditor $v_mID)
+                  VALUES(
+                    '$v_subject','$v_detail','$v_description','$v_price','$v_cate','$v_type','$v_photo1','$v_photo2','$v_photo3','$v_photo4','$v_photo5',
+                    '$n_name','$n_address','$n_province','$n_phone','$n_email','1',now(),'1',
+                    '$v_comment','$v_type_product','$v_day','$n_line','$n_code_edit' $m_mID
+                  )";
+    if (insert_tb($SqlInsert)==true) {
+      $jID = "";
+      $SqlSelect = "SELECT MAX(jID) as jID FROM sb_job LIMIT 1;";
+      foreach (select_tb($SqlSelect) as $kue) {
+        $jID = $kue['jID'];
+      }
+  		echo fSuccess(1,"ลงประกาศสำเร็จ",$LinkWeb."post/".$jID,2);
+  		//log_insert("เพิ่มประกาศใหม่ สำเร็จ",$_COOKIE[$CookieID]);
+  	}else {
+  		echo fError(1,"ประกาศไม่สำเร็จ กรุณาตรวจสอบข้อมูล",$SqlInsert);
+  		//log_insert("เพิ่มประกาศใหม่ ไม่สำเร็จ",$_COOKIE[$CookieID]);
+  	}
+
+
   }
-
-
 ?>
 <div class="col-xs-12">
   <form class="" action="<?php echo $LinkPath;?>" method="post" enctype="multipart/form-data">
@@ -30,9 +125,9 @@
           </div>
         </div>
         <div class="form-group col-xs-12 pr-0 pl-0">
-          <label class="control-label col-sm-3 text-ll pr-0 pl-0" for="province">จังหวัด:</label>
+          <label class="control-label col-sm-3 text-ll pr-0 pl-0" for="post_province">จังหวัด:</label>
           <div class="col-sm-6 col-xs-12 pr-0 pl-0">
-            <select class="form-control" name="province" required>
+            <select class="form-control" name="post_province" required>
               <option value="">เลือกจังหวัด</option>
               <?php
                 $SqlSelect = "SELECT *
@@ -93,8 +188,8 @@
         <div class="form-group col-xs-12 pr-0 pl-0">
           <label class="control-label col-sm-3 text-ll pr-0 pl-0" for="">สภาพสินค้า</label>
           <div class="col-sm-9 pr-0 pl-0">
-            <select class="form-control" name="">
-                <option value="">ไม่ระบุ</option>
+            <select class="form-control" name="post_product">
+                <option value="0">ไม่ระบุ</option>
                 <option value="1">ใหม่</option>
                 <option value="2">มือสอง</option>
                 <option value="3">ล้างสต๊อก</option>
@@ -104,8 +199,8 @@
         <div class="form-group col-xs-12 pr-0 pl-0">
           <label class="control-label col-sm-3 text-ll pr-0 pl-0" for="">จำนวนวันประกาศ</label>
           <div class="col-sm-9 pr-0 pl-0">
-            <select class="form-control" name="">
-                <option value="">ไม่จำกัด</option>
+            <select class="form-control" name="post_day">
+                <option value="0">ไม่จำกัด</option>
                 <option value="30">30 วัน</option>
                 <option value="90">90 วัน</option>
                 <option value="365">365 วัน</option>
@@ -113,11 +208,11 @@
           </div>
         </div>
         <div class="form-group col-xs-12 pr-0 pl-0">
-          <label class="control-label col-sm-3 text-ll pr-0 pl-0" for="post_subject">สถานะ</label>
+          <label class="control-label col-sm-3 text-ll pr-0 pl-0" for="post_comment">สถานะ</label>
           <div class="col-sm-9 pr-0 pl-0">
-            <select class="form-control" name="" required>
-                <option value="">เปิดให้ Comment</option>
-                <option value="">ปิด Comment</option>
+            <select class="form-control" name="post_comment" required>
+                <option value="1">เปิดให้ Comment</option>
+                <option value="0">ปิด Comment</option>
             </select>
           </div>
         </div>
@@ -160,19 +255,19 @@
         <div class="form-group col-xs-12 pr-0 pl-0">
           <label class="control-label col-sm-3 text-ll pr-0 pl-0" for="">ชื่อ - นามสกุล</label>
           <div class="col-sm-9 pr-0 pl-0">
-            <input type="text" class="form-control" name="" placeholder="กรอกชื่อ และ นามสกุล" autocomplete="off">
+            <input type="text" class="form-control" name="post_n_fullname" placeholder="กรอกชื่อ และ นามสกุล" autocomplete="off">
           </div>
         </div>
         <div class="form-group col-xs-12 pr-0 pl-0">
           <label class="control-label col-sm-3 text-ll pr-0 pl-0" for="">ที่อยู่</label>
           <div class="col-sm-9 pr-0 pl-0">
-            <textarea class="form-control" placeholder="กรอกที่อยู่" required autocomplete="off"></textarea>
+            <textarea class="form-control" name="post_n_address" placeholder="กรอกที่อยู่" required autocomplete="off"></textarea>
           </div>
         </div>
         <div class="form-group col-xs-12 pr-0 pl-0">
           <label class="control-label col-sm-3 text-ll pr-0 pl-0" for="">จังหวัด</label>
           <div class="col-sm-6 pr-0 pl-0">
-            <select class="form-control" name="" required>
+            <select class="form-control" name="post_n_province" required>
                 <option value="">เลือกจังหวัด</option>
                 <?php
                   $SqlSelect = "SELECT *
@@ -190,26 +285,26 @@
         <div class="form-group col-xs-12 pr-0 pl-0">
           <label class="control-label col-sm-3 text-ll pr-0 pl-0" for="">เบอร์ติดต่อ</label>
           <div class="col-sm-6 pr-0 pl-0">
-            <input type="text" class="form-control" name="" required placeholder="เบอร์ติดต่อกลับ" autocomplete="off">
+            <input type="text" class="form-control" name="post_n_telephone" required placeholder="เบอร์ติดต่อกลับ" autocomplete="off">
           </div>
         </div>
         <div class="form-group col-xs-12 pr-0 pl-0">
           <label class="control-label col-sm-3 text-ll pr-0 pl-0" for="">Email</label>
           <div class="col-sm-9 pr-0 pl-0">
-            <input type="text" class="form-control" name="" required placeholder="email สำหรับติดต่อ" autocomplete="off">
+            <input type="text" class="form-control" name="post_n_email" required placeholder="email สำหรับติดต่อ" autocomplete="off">
           </div>
         </div>
         <div class="form-group col-xs-12 pr-0 pl-0">
-          <label class="control-label col-sm-3 text-ll pr-0 pl-0" for="">LINE ID</label>
+          <label class="control-label col-sm-3 text-ll pr-0 pl-0" for="post_n_lineid">LINE ID</label>
           <div class="col-sm-6 pr-0 pl-0">
-            <input type="text" class="form-control" name="" placeholder="LINE ID" autocomplete="off">
+            <input type="text" class="form-control" name="post_n_lineid" placeholder="LINE ID" autocomplete="off">
           </div>
         </div>
 
         <div class="form-group col-xs-12 pr-0 pl-0">
           <label class="control-label col-sm-3 text-ll pr-0 pl-0" for="">รหัสแก้ไข</label>
           <div class="col-sm-6 pr-0 pl-0">
-            <input type="text" class="form-control" name="" required placeholder="รหัส สำหรับแก้ไขประกาศ" autocomplete="off">
+            <input type="number" class="form-control" name="post_code_edit" required placeholder="รหัส สำหรับแก้ไขประกาศ ตัวเลขเท่านั้น" autocomplete="off">
           </div>
         </div>
 
@@ -240,14 +335,6 @@
             <button type="submit" name="btnPost" class="btn btn-success btn-lg">บันทึกประกาศ</button>
           </div>
         </div>
-
-
-
-
-
-
-
-
       </div>
     </div>
 
