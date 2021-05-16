28f42e5bc1bc15f22fb2fdc7d533cfb0ccbbef1d
sinsand <seen@startconcept.com>
2021-05-16 14:53:27 +0700

    Update test.php

--- test.php
+++ test.php
@@ -3,7 +3,7 @@ include('config/config.php');
 
 $SqlSelect = "SELECT a.jID,a.jProvince,b.PROVINCE_ID FROM sb_job a LEFT OUTER JOIN p_province b ON (a.jProvince = b.PROVINCE_NAME) ORDER BY a.jProvince ASC";
 foreach (select_tb($SqlSelect) as $value) {
-  echo "<p> UPDATE sb_job SET jProvince = '".$value['PROVINCE_ID']."' WHERE jID = '".$value['jID']."'; /// ".$value['jProvince']." </p>";
+  echo "<p> UPDATE sb_job SET jProvince = '".$value['PROVINCE_ID']."' WHERE jID = '".$value['jID']."'; ## ".$value['jProvince']." </p>";
 }
 
 ?>
