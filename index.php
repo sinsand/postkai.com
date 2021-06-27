9c92d4d38a2ce7611d77357bcdc9e646e910a54d
sinsand <seen@startconcept.com>
2021-06-27 20:12:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ include("config/config.php");
 if ($UrlPage!="") {
   if ($UrlPage=="sitemap.xml") {
     include("view-sitemap.php");
-  }else if ($UrlPage=="member"){
-    include("system/member/index.php");
+  //}else if ($UrlPage=="member"){
+    //include("system/member/index.php");
   }else {
     include("system/index.php");
   }
