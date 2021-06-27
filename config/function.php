6a23a220b30cede18c52a887010c776efae2bc7a
sinsand <seen@startconcept.com>
2021-06-27 21:12:16 +0700

    Update function.php

--- config/function.php
+++ config/function.php
@@ -67,7 +67,7 @@ function log_insert($value,$eid){
 
 ///// check login
 function checklogin(){
-  if (empty($_COOKIE[$CookieID]){
+  if (empty($_COOKIE[$CookieID])){
     echo header("Location:$LinkWeb");
   }
 }
