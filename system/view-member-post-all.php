f88338e9e600288a6f663b5b79cb94e82c09fced
sinsand <seen@startconcept.com>
2021-06-27 21:47:35 +0700

    Update view-member-post-all.php

--- system/view-member-post-all.php
+++ system/view-member-post-all.php
@@ -5,7 +5,7 @@
  if (select_num($SqlSelect)>0) {
    foreach (select_tb($SqlSelect) as $row) {
      ?>
-      <div class="row">
+      <div class="row pb-10" style="border-bottom: 1px solid #e1e1e1;">
         <div class="col-xs-6 col-sm-2 col-md-2 text-left fw-b">#ID <?php echo $row['IDJOB'];?></div>
         <div class="col-sm-7 hidden-xs"><?php echo $row['jTitle'];?></div>
         <div class="col-xs-6 col-sm-3 col-md-3 text-right">
@@ -15,7 +15,7 @@
             <button id="<?php echo $row['jID'];?>" data-toggle="modal" class="btn btn-sm btn-default modal-trash" data-target="#modal-trash"><i class="fa fa-trash"></i></button>
           </div>
         </div>
-        <div class="col-xs-12 hidden-sm hidden-md hidden-lg"><?php echo $row['jTitle'];?></div>
+        <div class="col-xs-12 pt-5 hidden-sm hidden-md hidden-lg"><?php echo $row['jTitle'];?></div>
       </div>
      <?php
    }
