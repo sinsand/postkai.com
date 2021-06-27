52ed05463ea996f551c01e51b2b525acc53ca7c4
sinsand <seen@startconcept.com>
2021-06-27 20:56:27 +0700

    Update view-member-profile.php

--- system/view-member-profile.php
+++ system/view-member-profile.php
@@ -29,52 +29,52 @@ if (isset($_POST['btnUpdate'])) {
       <div class="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2">
         <form class="form-horizontal" action="<?php echo $LinkPath;?>" method="post">
           <div class="form-group">
-            <label class="control-label col-sm-2" for="email">อีเมล:</label>
-            <div class="col-sm-10">
+            <label class="control-label col-sm-3" for="email">อีเมล:</label>
+            <div class="col-sm-9">
               <input type="email" class="form-control" id="email" value="<?php echo $m_id;?>" readonly>
             </div>
           </div>
           <div class="form-group">
-            <label class="control-label col-sm-2" for="password">รหัสผ่าน:</label>
-            <div class="col-sm-10">
+            <label class="control-label col-sm-3" for="password">รหัสผ่าน:</label>
+            <div class="col-sm-9">
               <input type="password" class="form-control" id="password" placeholder="รหัสผ่าน" required autocomplete="off">
             </div>
           </div>
           <div class="form-group">
-            <label class="control-label col-sm-2" for="password">ยืนยัน รหัสผ่าน:</label>
-            <div class="col-sm-10">
+            <label class="control-label col-sm-3" for="password">ยืนยัน รหัสผ่าน:</label>
+            <div class="col-sm-9">
               <input type="password" class="form-control" id="confirm-password" placeholder="ยืนยัน รหัสผ่านอีกครั้ง" required autocomplete="off">
             </div>
           </div>
           <div class="form-group">
-            <label class="control-label col-sm-2" for="email">ชื่อจริง:</label>
-            <div class="col-sm-10">
+            <label class="control-label col-sm-3" for="email">ชื่อจริง:</label>
+            <div class="col-sm-9">
               <input type="text" class="form-control" id="fname" value="<?php echo $m_fname;?>" placeholder="ชื่อจริง" required autocomplete="off">
             </div>
           </div>
           <div class="form-group">
-            <label class="control-label col-sm-2" for="email">นามสกุล:</label>
-            <div class="col-sm-10">
+            <label class="control-label col-sm-3" for="email">นามสกุล:</label>
+            <div class="col-sm-9">
               <input type="text" class="form-control" id="lname" value="<?php echo $m_lname;?>" placeholder="นามสกุล" required autocomplete="off">
             </div>
           </div>
           <div class="form-group">
-            <label class="control-label col-sm-2" for="email">ที่อยู่:</label>
-            <div class="col-sm-10">
+            <label class="control-label col-sm-3" for="email">ที่อยู่:</label>
+            <div class="col-sm-9">
               <textarea name="address" placeholder="ที่อยู่" class="form-control" rows="8">
                 <?php echo $m_address;?>
               </textarea>
             </div>
           </div>
           <div class="form-group">
-            <label class="control-label col-sm-2" for="email">รหัสไปรษณีย์:</label>
-            <div class="col-sm-10">
+            <label class="control-label col-sm-3" for="email">รหัสไปรษณีย์:</label>
+            <div class="col-sm-9">
               <input type="text" class="form-control" id="postcode" value="<?php echo $m_postcode;?>" placeholder="รหัสไปรษณีย์" required autocomplete="off">
             </div>
           </div>
           <div class="form-group">
-            <label class="control-label col-sm-2" for="email">เบอร์ติดต่อ:</label>
-            <div class="col-sm-10">
+            <label class="control-label col-sm-3" for="email">เบอร์ติดต่อ:</label>
+            <div class="col-sm-9">
               <input type="text" class="form-control" id="phone" value="<?php echo $m_telephone;?>" placeholder="เบอร์ติดต่อ" required autocomplete="off">
             </div>
           </div>
@@ -82,7 +82,7 @@ if (isset($_POST['btnUpdate'])) {
 
 
           <div class="form-group">
-            <label class="control-label col-sm-2"></label>
+            <label class="control-label col-sm-3"></label>
             <div class="col-sm-10">
               <button type="submit" name="btnUpdate" class="btn btn-success">บันทึกข้อมูล</button>
             </div>
