80b6d3d6ed88e02c0577640cf93c7255809213cd
sinsand <seen@startconcept.com>
2021-06-27 20:16:28 +0700

    Update view-member.php

--- system/view-member.php
+++ system/view-member.php
@@ -1,19 +1,30 @@
-<ul class="nav nav-tabs">
-  <li class="active"><a href="#profile">Profile</a></li>
-  <li><a href="#postall">Post All</a></li>
-  <li><a href="#commentall">Comment All</a></li>
-</ul>
-<div class="tab-content">
-  <div id="profile" class="tab-pane fade in active">
-    <h3>Profile</h3>
-    <p>Some content.</p>
-  </div>
-  <div id="postall" class="tab-pane fade">
-    <h3>Post All</h3>
-    <p>Some content in menu 1.</p>
-  </div>
-  <div id="commentall" class="tab-pane fade">
-    <h3>Comment All</h3>
-    <p>Some content in menu 2.</p>
+<h2 class="main-head-cate t-search f-k">Profile</h2>
+<div class="col-xs-12 p-0">
+  <ul class="nav nav-tabs f-k fs-13">
+    <li class="active"><a href="#profile">Profile</a></li>
+    <li><a href="#postall">Post All</a></li>
+    <li><a href="#commentall">Comment All</a></li>
+  </ul>
+  <div class="tab-content">
+    <div id="profile" class="tab-pane fade in active">
+      <div class="row m-0">
+        <div class="col-xs-12" style="padding:20px;">
+        </div>
+      </div>
+    </div>
+
+    <div id="postall" class="tab-pane">
+      <div class="row m-0">
+        <div class="col-xs-12" style="padding:20px;">
+        </div>
+      </div>
+    </div>
+
+    <div id="commentall" class="tab-pane">
+      <div class="row m-0">
+        <div class="col-xs-12" style="padding:20px;">
+        </div>
+      </div>
+    </div>
   </div>
 </div>
