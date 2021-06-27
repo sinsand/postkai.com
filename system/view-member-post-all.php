ca54a9d6edce8d678573078253580ca7a2918159
sinsand <seen@startconcept.com>
2021-06-27 21:29:00 +0700

    Update view-member-post-all.php

---
+++ system/view-member-post-all.php
@@ -0,0 +1,22 @@
+<?php
+ $SqlSelect = "SELECT *
+               FROM sb_job
+               WHERE ( mID = '".base64url_decode($_COOKIE[$CookieID])."' )";
+ if (select_num($SqlSelect)>0) {
+   foreach (select_tb($SqlSelect) as $row) {
+     ?>
+      <div class="row">
+        <div class="col-sm-3"><?php echo $row['IDJOB'];?></div>
+        <div class="col-sm-5"><?php echo $row['Title'];?></div>
+        <div class="col-sm-4 text-center">
+          <div class="btn-group btn-xs">
+            <button id="<?php echo $row['jID'];?>" data-toggle="modal" class="btn btn-sm btn-default modal-view" data-target="#modal-view"><i class="fa fa-search"></i></button>
+            <button id="<?php echo $row['jID'];?>" data-toggle="modal" class="btn btn-sm btn-default modal-edit" data-target="#modal-edit"><i class="fa fa-edit"></i></button>
+            <button id="<?php echo $row['jID'];?>" data-toggle="modal" class="btn btn-sm btn-default modal-trash" data-target="#modal-trash"><i class="fa fa-trash"></i></button>
+          </div>
+        </div>
+      </div>
+     <?php
+   }
+ }
+?>
